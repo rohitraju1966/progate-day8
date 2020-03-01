18e12a1f9a99af3a313f8711a25009d01366a9a6
Rohit.r <rohitraju1966@users.noreply.github.com>
2020-03-01 23:04:58 +0530

    updated

--- script.js
+++ script.js
@@ -1,77 +1,78 @@
-const calculatorScreen = document.querySelector('.calculator-screen');
+let prevInput='0';
+let calculationOperator='';
+let currentInput='0';
+
+const inputNumber=(number) => {
+    if (currentInput === '0') {
+        currentInput=number;
+    }
+    else {
+        currentInput+=number;
+    }    
+}
+
+const calculatorsScreen=document.querySelector('.calculator_screen')
 
 const updateScreen = (number) => {
-    calculatorScreen.value = number;
+    calculatorsScreen.value=number;     
 }
 
-const numbers = document.querySelectorAll(".number");
+const numbers=document.querySelectorAll(".number");
 
-let prevInput = 0;
-let calculationOperator = '';
-let currentInput = 0;
-
-const inputNumber = (number) => {
-    if(currentInput === '0')
-    {
-        currentInput = number;
-    }
-    else 
-    {
-        currentInput += number;
-    }
-}
-
-numbers.forEach((number) => 
-{
-    number.addEventListener("click", (event) => {
+numbers.forEach((number)=>{
+    number.addEventListener("click",(event) => {
         inputNumber(event.target.value);
         updateScreen(currentInput);
     })
 })
 
-const operators = document.querySelectorAll(".operator");
 
-operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value);
-    })
-})
-
-const inputOperator = (operator) => {
-    prevInput = currentInput;
-    calculationOperator = operator;
-    currentInput = '0';
+const inputOperator=(operator) => {
+    prevInput=currentInput;
+    calculationOperator=operator;
+    currentInput='0';
 }
 
-// operators.addEventListener("click", (event) => {
-//     inputOperator(event.target.value);
-// })
+const operators=document.querySelectorAll(".operator");
 
-const equalSign = document.querySelector('.equal-sign');
+operators.forEach((operator)=>{
+    operator.addEventListener("click",(event) => {
+            inputOperator(event.target.value);
+    })
+})   
 
-equalSign.addEventListener('click', () => {
-    calculate();
-    updateScreen(currentInput);
-})
-
-const calculate = () => {
+const calculate=() => {
     let result = 0;
     switch(calculationOperator) {
         case '+':
-            result = prevInput + currentInput;
-            break;
+            result = parseInt(prevInput) + parseInt(currentInput);
+            break
         case '-':
-            result = prevInput - currentInput;
-            break;
-        case 'x':
-            result = prevInput * currentInput;
-            break;
+            result = parseInt(prevInput) - parseInt(currentInput);
+            break
+        case '*':
+            result = parseInt(prevInput) * parseInt(currentInput);
+            break
         case '/':
-            result = prevInput / currentInput;
-            break;
+             result = parseInt(prevInput) / parseInt(currentInput);
+            break
         default:
-            return result;
+            return                
     }
-    currentInput = result.toString();
-    calculationOperator = '';
+    currentInput = result.toString()
+    calculationOperator=''
 }
+
+const equalSign=document.querySelector('.equal_sign')
+
+equalSign.addEventListener('click',() => {
+    calculate()
+    updateScreen(currentInput)
+})
+
+const allClear=document.querySelector('.all-clear')
+
+allClear.addEventListener('click',() => {
+    currentInput='0'
+    updateScreen(currentInput)
+})
